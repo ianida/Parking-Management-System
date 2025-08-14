e60bbf44da25f60b02318ddd471ed1d63df5de98
rojarmhj <rojarmhj@gmail.com>
2025-08-14 17:56:01 +0545

    config changes

--- config/config.php
+++ config/config.php
@@ -1,3 +1,7 @@
 <?php
 // config.php
 define('BASE_URL', 'http://localhost:8011/Parking-Management-System/');
+
+// Khalti API keys (sandbox for testing)
+define('KHALTI_PUBLIC_KEY', '8ec25258bb9445b6bef8f40e0eff4048');
+define('KHALTI_SECRET_KEY', 'f3364008386244739ed8d7727fff7de0');
